ffb80bcd8e1d6e433a16c996273ea9d0c947bb29
joelricmisa <joelriccmisa@gmail.com>
2023-11-02 21:32:13 +0800

    update allowed origins

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,6 +1,3 @@
-const allowedOrigins = [
-    "http://127.0.0.1:5000", 
-    "http://localhost:5000"
-];
+const allowedOrigins = ["http://127.0.0.1:5000", "http://localhost:5000", "http://localhost:5173"];
 
 module.exports = allowedOrigins;
