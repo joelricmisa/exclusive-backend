b78d448fe0f39be586a76e9dd4d8697d3ec9a3f4
joelricmisa <joelriccmisa@gmail.com>
2023-11-26 00:27:36 +0800

    added description and rate count in product schema

--- models/Product.js
+++ models/Product.js
@@ -5,10 +5,12 @@ const productSchema = new Schema(
 	{
 		name: { type: String, required: true },
 		image: String,
-		price: { type: String, required: true },
+		price: { type: Number, required: true },
 		discount: Number,
 		quantity: { type: Number, default: 1 },
+		description: String,
 		rating: String,
+		rate_count: Number,
 		categories: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
