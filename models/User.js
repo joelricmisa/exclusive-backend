3107339a0fdb192b9e86b5cad59fa7f34bc351fd
joelricmisa <joelriccmisa@gmail.com>
2023-11-30 23:17:06 +0800

    added wishlist and cart in user model

--- models/User.js
+++ models/User.js
@@ -16,6 +16,18 @@ const userSchema = new Schema(
 			editor: Number,
 			admin: Number,
 		},
+		wishlist: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Product",
+			},
+		],
+		cart: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Product",
+			},
+		],
 	},
 	{ timestamps: true }
 );
