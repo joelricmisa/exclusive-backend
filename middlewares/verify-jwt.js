46dd28dbce1ebc42db28971ab067372d0e29512f
joelricmisa <joelriccmisa@gmail.com>
2023-12-13 23:53:30 +0800

    turn my console logs into comments

--- middlewares/verify-jwt.js
+++ middlewares/verify-jwt.js
@@ -5,15 +5,15 @@ const verifyJWT = async (req, res, next) => {
 		const authHeader = req.headers.authorization;
 		if (!authHeader.startsWith("Bearer ")) return res.sendStatus(401);
 		const token = authHeader.split(" ")[1];
-		console.log(token);
+		// console.log(token);
 
 		jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
 			if (err) return res.sendStatus(403);
 			req.user = decoded.userInfo.name;
 			req.roles = decoded.userInfo.role;
 			req.id = decoded.userInfo.id;
-			console.log(req.roles);
-			console.log(req.id);
+			// console.log(req.roles);
+			// console.log(req.id);
 			next();
 		});
 	} catch (err) {
--- middlewares/verify-role.js
+++ middlewares/verify-role.js
@@ -4,7 +4,7 @@ const verifyRole = (...allowedRoles) => {
 			if (!req?.roles) return res.send(400).json({ message: "No role found" });
 			const rolesArray = [...allowedRoles];
 			const result = rolesArray.map((role) => req?.roles.includes(role)).find((val) => val === true);
-			console.log(`Authorized:`, result);
+			// console.log(`Authorized:`, result);
 			if (!result) return res.send(401).json({ message: "you don't have access for this" });
 			next();
 		};
