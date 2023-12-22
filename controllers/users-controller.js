1f35abb75e2a5fc762bd94906e9967ee4915f5c4
joelricmisa <joelriccmisa@gmail.com>
2023-12-22 21:04:48 +0800

    refactor(user controller): cart methods

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -44,7 +44,7 @@ const getCurrentUser = async (req, res) => {
 		const id = req.id;
 		if (!id) return res.sendStatus(401);
 
-		const user = await User.findById(id, "-password -__v").exec();
+		const user = await User.findById(id, "-password -__v").populate({ path: "cart.product_id" }).exec();
 		if (!user) return res.sendStatus(401);
 
 		res.status(200).json({
@@ -90,7 +90,7 @@ const handleNewUser = async (req, res) => {
 
 const updateUserById = async (req, res) => {
 	try {
-		const { name, email, cartIds, wishlistIds } = req.body;
+		const { name, email, wishlistIds } = req.body;
 		const userId = req.params.id;
 
 		if (!userId) return res.status(400).json({ message: "User id is required" });
@@ -101,20 +101,7 @@ const updateUserById = async (req, res) => {
 
 		if (!name || !email) return res.status(400).json({ message: "Please fill up all inputs" });
 
-		if (cartIds) {
-			// console.log("cartIds", cartIds);
-
-			const validObjectCartIds = cartIds.map((id) => new mongoose.Types.ObjectId(id));
-
-			const products = await Product.find({ _id: { $in: validObjectCartIds } }).exec();
-
-			const cartIdsToAdd = products.map((product) => product._id);
-
-			await User.updateOne({ _id: userId }, { ...req.body });
-
-			await User.updateOne({ _id: userId }, { $addToSet: { cart: { $each: cartIdsToAdd } } });
-			//
-		} else if (wishlistIds) {
+		if (wishlistIds) {
 			const objectWishlistIds = wishlistIds.map((id) => new mongoose.Types.ObjectId(id));
 
 			const products = await Product.find({ _id: { $in: objectWishlistIds } }).exec();
@@ -141,27 +128,6 @@ const updateUserById = async (req, res) => {
 		});
 	} catch (err) {
 		res.status(500).json({ error: err.message });
-	}
-};
-
-const removeProductFromCart = async (req, res) => {
-	try {
-		const userId = req.params.id;
-		const productId = req.params.productId;
-
-		await User.updateOne({ _id: userId }, { $pull: { cart: productId } }, { new: true });
-
-		const user = await User.findById(userId).populate("cart").exec();
-
-		res.status(200).json({
-			message: `Product removed from cart successfully`,
-			status: "ok",
-			status_code: 200,
-			data: user,
-		});
-	} catch (err) {
-		console.log(err);
-		return res.status(500).json({ error: err.message });
 	}
 };
 
@@ -207,6 +173,75 @@ const deleteUserById = async (req, res) => {
 	}
 };
 
+const updateCart = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const { product_id, quantity } = req.body;
+
+		if (!product_id || !quantity) {
+			return res.status(400).json({ error: "Product ID and quantity are required" });
+		}
+
+		const user = await User.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const product = await Product.findById(product_id);
+
+		if (!product) {
+			return res.status(404).json({ error: "Product not found" });
+		}
+
+		const existingCartItem = user.cart.find((item) => item.product_id.equals(product_id));
+
+		if (existingCartItem) {
+			existingCartItem.quantity = quantity;
+			existingCartItem.price = (Number(product.price) - Number(product.price) * Number(product?.discount / 100)) * product.quantity;
+		} else {
+			user.cart.push({
+				product_id,
+				quantity,
+				price: (Number(product.price) - Number(product.price) * Number(product?.discount / 100)) * product.quantity,
+			});
+		}
+
+		await user.save().then((data) => data.populate({ path: "cart.product_id" }));
+
+		res.json(user);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+const removeCartItem = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const { product_id } = req.body;
+
+		if (!product_id) {
+			return res.status(400).json({ error: "Product ID is required" });
+		}
+
+		const user = await User.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		user.cart = user.cart.filter((item) => !item.product_id.equals(product_id));
+
+		await user.save().then((data) => data.populate({ path: "cart.product_id" }));
+
+		res.json(user);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
 module.exports = {
 	getAllUsers,
 	getUserById,
@@ -214,6 +249,7 @@ module.exports = {
 	handleNewUser,
 	updateUserById,
 	deleteUserById,
-	removeProductFromCart,
 	removeProductFromWishlist,
+	updateCart,
+	removeCartItem,
 };
