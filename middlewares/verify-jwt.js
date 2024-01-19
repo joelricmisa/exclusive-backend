c608c1cf5bd2c260560f88aea2d9b004497ac390
joelricmisa <joelriccmisa@gmail.com>
2024-01-19 16:45:12 +0800

    auth

--- middlewares/verify-jwt.js
+++ middlewares/verify-jwt.js
@@ -2,12 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = async (req, res, next) => {
 	try {
-		const BearerToken = req.headers["authorization"];
-		// console.log(BearerToken);
-		if (!BearerToken) {
-			return res.status(401).json({ error: "Unauthorized" });
-		}
-		const token = BearerToken.replace(/^Bearer\s/, "");
+		// const BearerToken = req.headers["authorization"];
+		// if (!BearerToken) {
+		// 	return res.status(401).json({ error: "Unauthorized" });
+		// }
+		// const token = BearerToken.replace(/^Bearer\s/, "");
+
+		const authHeader = req.headers.authorization;
+		if (!authHeader.startsWith("Bearer ")) return res.sendStatus(401);
+		const token = authHeader.split(" ")[1];
 
 		jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
 			if (err) return res.sendStatus(403);
