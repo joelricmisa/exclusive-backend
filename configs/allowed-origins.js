c7836cfc37cd12740f6a6c1c504cdb56d69a063a
joelricmisa <joelriccmisa@gmail.com>
2024-01-19 13:47:43 +0800

    add my prod domain in my allowed origins

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["http://127.0.0.1:5000", "http://localhost:5000", "http://localhost:5173"];
+const allowedOrigins = [
+	"http://127.0.0.1:5000",
+	"http://localhost:5000",
+	"http://localhost:5173",
+	"https://exclusive-shop-joelric.vercel.app",
+];
 
 module.exports = allowedOrigins;
